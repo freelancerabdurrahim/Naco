896e8a4e5732347803e1190563ef433f26f86407
Abdur Rahim <abdurrahim300431@gmail.com>
2023-03-11 11:12:55 +0600

    Naco Projects

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,46 @@
 $(document).ready(function(){
     $('.carousel_area_wrapper').owlCarousel({
-        items:3.5,
+        items:4,
         loop:true,
         nav:true,
         margin:20,
         dots:false,
         nav:true,
-        navText:['<i class="fas fa-angle-left"></i>' ,'<i class="fas fa-angle-right"></i>']
+        navText:['<i class="fas fa-angle-left"></i>' ,'<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            700:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
     })
 
 
     $('.bestProduts_carousel').owlCarousel({
-        items:3.5,
+        items:4,
         loop:true,
         nav:true,
         margin:20,
         dots:false,
         nav:true,
-        navText:['<i class="fas fa-angle-left"></i>' ,'<i class="fas fa-angle-right"></i>']
+        navText:['<i class="fas fa-angle-left"></i>' ,'<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            700:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+        
     })
 
 
